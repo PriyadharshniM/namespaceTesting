0e5251f7f3ada2e89c0586a808fd8b8c387793c2
PriyadharshniM <Priyadharshni.murugan@syncfusion.com>
2023-08-01 14:26:01 +0530

    Added CustomLibrary

---
+++ CustomLibrary/Class1.cs
@@ -0,0 +1,10 @@
+﻿[assembly: XmlnsDefinition("http://schemas.custom.com/maui", "CustomLibrary")]
+
+namespace CustomLibrary
+{
+    // All the code in this file is included in all platforms.
+    public class CustomControl: Entry
+    {
+
+    }
+}
